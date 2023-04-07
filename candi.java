24d2cd186a3fd3cb1f465e938de2e072cf6f1ebe
sagar-1004 <82384768+sagar-1004@users.noreply.github.com>
2023-04-07 12:55:26 +0530

    Update candi.java

--- candi.java
+++ candi.java
@@ -3,24 +3,54 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.*;
+import javafx.util.Pair;
+import java.util.Pair;
 
 public class candi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String csvFile = "src/main/resources/candidateFile.csv";
-        String line = "";
-        String csvDelimiter = ",";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(csvDelimiter);
-                for (String value : values) {
-                    System.out.print(value + " ");
-                }
-                System.out.println();
+		try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/candidateFile.csv"));
+            ArrayList<String[]> lines = new ArrayList<String[]>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] elements = line.split(",");
+                lines.add(elements);
             }
-        } catch (IOException e) {
+            reader.close();
+            
+            String[][] csvArray = new String[lines.size()][];
+            for (int i = 0; i < lines.size(); i++) {
+                csvArray[i] = lines.get(i);
+            }
+            
+//            Map<String, Map<String, Integer>>candy = new HashMap<String, Integer>;
+            Map<String, Map<String, Integer>> consti = new HashMap<>();
+            Pair<Integer, String> p = new Pair<>();
+            Vector<p> vector= new Vector<>();
+            // do something with csvArray
+            for(int i=1;i<lines.size();i++)
+            {
+            	Map<String, Integer> candid = new HashMap<>();
+            	candid.put(csvArray[i][1], 0);
+            	consti.put(csvArray[i][0], candid);
+            	
+            }
+            
+            for (Map.Entry<String, Map<String, Integer>> c : consti.entrySet()) {
+            	  Map<String, Integer> addMap = c.getValue();
+
+            	  // Iterate InnerMap
+            	  for (Map.Entry<String, Integer> addressSet : addMap.entrySet()) {
+            	    System.out.println(addressSet.getKey() + " :: " + addressSet.getValue());
+            	  }
+            	}
+            
+            
+        } catch (Exception e) {
+            System.err.format("Exception occurred trying to read '%s'.", "path/to/csv/file.csv");
             e.printStackTrace();
         }
 
